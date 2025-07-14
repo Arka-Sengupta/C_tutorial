e87714c9a8403ca8bfa96d918e4a6801e80f17d8
Arka-Sengupta <arka.sengupta.06@gmail.com>
2025-07-14 13:54:03 +0530

    variables

---
+++ 1-Variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// variables in C are similar to that of JAVA
+int age = 19;
+float salary = 7.2f;
+double cgpa = 8.5;
+int main(){
+    printf("You are %d years old\n",age);
+    printf("Your salary is Rs.%f\n",salary);
+    printf("Your cgpa is %f\n",cgpa);
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+//this is a comment
+/*
+This 
+    is
+         a 
+            multi 
+                    line 
+                            comment
+*/
 int main(){
-    printf("this is a C program");
+    printf("this is a C program\n");
+    printf("This is a new line");
     return 0;
 }
