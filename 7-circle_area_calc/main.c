b12c707f4ac72b7660cf1a492785ea2e0443d751
Arka-Sengupta <arka.sengupta.06@gmail.com>
2025-07-15 18:54:52 +0530

    new addition

--- 7-circle_area_calc/main.c
+++ 7-circle_area_calc/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 int main(){
-    
+    double radius = 0.0;
+    double area = 0.0;
+    printf("Enter the radius of the circle : ");
+    scanf("%lf",&radius);
+    area = 3.14 * pow(radius,2);
+    printf("The area of the circle is : %.2lf",area);
+    return 0;
 }
